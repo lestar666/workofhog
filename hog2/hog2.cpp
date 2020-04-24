b3767f5a3fff579c7991663c4d5d76bac4297faa
2077946749 <2077946749@qq.com>
2020-04-24 21:48:42 +0800

    0

--- hog2/hog2.cpp
+++ hog2/hog2.cpp
@@ -1,20 +1,74 @@
-﻿// hog2.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include<opencv2/opencv.hpp>
+#include<iostream>
+#include<math.h>
 
-#include <iostream>
+using namespace std;
+using namespace cv;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+float distance(float * l, float * m, int n) {
+	int totalnum = 0;
+	for (int i = 0; i < n; i++) {
+		totalnum = totalnum + pow((*(l + i) - *(m + i)), 2);
+	}
+	return totalnum;
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
+void hist(Mat src, int cell_Size, int nX, int nY, float * hist) {
+	Mat gx, gy;
+	Mat mag, angle;
+	Sobel(src, gx, CV_32F, 1, 0, 1);
+	Sobel(src, gy, CV_32F, 0, 1, 1);
+	cartToPolar(gx, gy, mag, angle, true);
+	int n = 0;
+	for (int i = 0; i < nY; i++) {
+		for (int j = 0; j < nX; j++) {
+			int n_x = j * cell_Size, n_y = i * cell_Size;
+			for (int l = n_x; l < cell_Size + n_x; l++) {
+				for (int m = n_y; m < cell_Size + n_y; m++) {
+					int region = (angle.at<float>(l, m) - 1) / 40;
+					*(hist + region + n) += mag.at<float>(l, m);
+				}
+			}
+			n += 9;
+		}
+	}
+}
 
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+int main() {
+
+	Mat src = imread("hogTemplate.jpg", 0);
+	Mat dst1 = imread("img1.jpg", 0);
+	Mat dst2 = imread("img2.jpg", 0);
+
+	int cell_Size = 16;
+	int nX = src.cols / cell_Size;
+	int nY = src.rows / cell_Size;
+	int bins = 9 * nX * nY;
+
+	float * src_hist = new float[bins];
+	memset(src_hist, 0, sizeof(float)*bins);
+	float * dst1_hist = new float[bins];
+	memset(dst1_hist, 0, sizeof(float)*bins);
+	float * dst2_hist = new float[bins];
+	memset(dst2_hist, 0, sizeof(float)*bins);
+
+	hist(src, cell_Size, nX, nY, src_hist);
+	hist(dst1, cell_Size, nX, nY, dst1_hist);
+	hist(dst2, cell_Size, nX, nY, dst2_hist);
+
+	int a = distance(src_hist, dst1_hist, bins);
+	int b = distance(src_hist, dst2_hist, bins);
+
+	delete[] src_hist;
+	delete[] dst1_hist;
+	delete[] dst2_hist;
+
+	cout << "the distance of former is " << a << endl;
+	cout << "the distance of later is  " << b << endl;
+
+	if (a > b)
+		cout << "the later is greater" << endl;
+	else
+		cout << "the former is greater" << endl;
+	return 0;
+}
